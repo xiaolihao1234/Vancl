a9700446e0a91587f4c3ea9afa019d7b83ca3a6a
xiaolihao1234 <1367497565@qq.com>
2017-06-07 09:07:41 +0800

    init commit

--- src/js/release/index.js
+++ src/js/release/index.js
@@ -1,15 +1,9 @@
 /**
  * Created by matrix on 2017/5/26.
  */
-require('../../css/lib/reset.css');
-require('../../css/page/style-one.css');
-require('../../css/page/style-two.css');
-require('../../css/page/style-three.css');
-require('../../css/page/style-four.css');
-require('../lib/jquery-2.1.1.min');
-require('../lib/vue.min');
-require('../module/utils');
-require('../page/render-one');
-require('../page/render-two');
-require('../page/render-three');
-require('../page/render-four');
+require('../../../build/cart.html');
+require('../../js/lib/jquery-2.1.1.min.js');
+require('../../css/page/cart.css');
+require('../../js/lib/vue.min.js');
+require('../../js/module/utils.js');
+require('../../js/page/cart.js');
